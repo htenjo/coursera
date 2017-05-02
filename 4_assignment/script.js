584da7d2ef4e34cfdcaf9c4de905f3c13a85a164
Hernan Tenjo <htenjo@hugeinc.com>
2017-05-02 13:03:17 -0500

    Delete flag.

--- 4_assignment/script.js
+++ 4_assignment/script.js
@@ -1,6 +1,4 @@
 (function () {
-  console.log("Hey I'm running.... ");
-
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   for (var i=0; i<names.length; i++) {
